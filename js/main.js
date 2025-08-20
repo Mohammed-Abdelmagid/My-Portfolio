5fff27c792953201647d8d504105e4e1c3ad5fac
Mohammed-Abdelmagid <gdbdhdhd@icloud.com>
2025-08-20 19:06:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let header = document.querySelector("header")
+window.onscroll=function(){
+    if (window.scrollY>=50){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+}
